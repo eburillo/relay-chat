dd4b61bfae2713206a0483fa8e4816832653f9c7
Enrique Burillo <enrique.burillo@gluckgames.com>
2017-02-21 23:39:23 +0100

    Conditional class for buttons with 'Classnames'

--- js/components/Entry/ControlButton.js
+++ js/components/Entry/ControlButton.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Relay from 'react-relay';
+import classNames from 'classnames';
 
 class ControlButton extends React.Component {
   render() {
-    let btnClass = "controls--button fa fa-" + this.props.icon;
+    const icon = this.props.icon;
+    const btnClass = classNames({
+      'controls--button': true,
+      'fa': true,
+      [`fa-${icon}`]: icon
+    });
     return (
       <button className={btnClass} onClick={this.props.clickHandler}></button>
     );
